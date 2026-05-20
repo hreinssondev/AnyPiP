7956b61f824b541e6de7343ed6dbf24f41cf94df
hreinssondev <hreinssondev@gmail.com>
2026-05-20 20:30:20 +0200

    Initial Commit

---
+++ AppsPIP/AppsPIPApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppsPIPApp.swift
+//  AppsPIP
+//
+//  Created by H on 20/05/2026.
+//
+
+import SwiftUI
+
+@main
+struct AppsPIPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
